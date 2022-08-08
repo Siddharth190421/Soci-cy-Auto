93d53b95cbdcfaa081b1340ed10da1d0c100c698
Siddharth190421 <siddharth.d@testriq.com>
2022-08-08 16:00:54 +0530

    Listing C3000-3

--- cypress/e2e/Listings.cy.js
+++ cypress/e2e/Listings.cy.js
@@ -2,6 +2,11 @@ import {ListingPage} from "../support/page_objects/Listings"
 
 const Listing = new ListingPage()
 
+
+let text = '';
+
+let textafter = '';
+
 describe('Listings', ()=>{
 
     beforeEach(() => {
@@ -37,15 +42,15 @@ describe('Listings', ()=>{
     it('Auto Sync Enable All - C2993', ()=>{
 
         Listing.settingsGear()
-        cy.get('.btn_enable_all_autosync').click()
-        cy.get('.DlgUtilityConfirm > .bbm-modal > .bbm-modal__bottombar > .primary_button').click()
+        Listing.enableAllListings()
+
     })
 
     it('Auto Sync Disable All - C2994', ()=>{
 
         Listing.settingsGear()
-        cy.get('.btn_disable_all_autosync').click()
-        cy.get('.DlgUtilityConfirm > .bbm-modal > .bbm-modal__bottombar > .primary_button').click()
+        Listing.disableAllListings()
+
     })
 
     it('Pause Auto Sync - C2995', ()=>{
@@ -58,8 +63,7 @@ describe('Listings', ()=>{
 
         Listing.settingsGear()
         //Step 1 Enable all listing
-        cy.get('.btn_enable_all_listings').click()
-        cy.get('.DlgUtilityConfirm > .bbm-modal > .bbm-modal__bottombar > .primary_button').click()
+        Listing.enableAllListings()
 
         Listing.gotoListingAccount()
 
@@ -72,8 +76,8 @@ describe('Listings', ()=>{
         cy.get('[style="--section-size:94px; --grid-gap:10px; --grid-template-columns:1fr;"] > .subsections > .LayoutSubSectionView > .LayoutDropAreaView > .BiModulesModule > .BiModulesTypesBase').should('be.visible')
         cy.get('.btn_action_bulk_edit').should('be.visible')
         cy.get('.multi_value_container').should('be.visible')
-        // cy.get('#DataTables_Table_3_wrapper').should('be.visible')
         cy.get('.dataTables_scrollBody').should('be.visible')
+
 
     })
 
@@ -94,18 +98,102 @@ describe('Listings', ()=>{
 
     })
 
-    it.only('Listing on Loactions Dash - C2999', ()=>{
+    it('Listing on Loactions Dash - C2999', ()=>{
 
         cy.visit('/admin/account/3854/office/0/project/320406/listings_overview/3182')
         cy.wait(6000)
 
-        cy.get('.highcharts-background').should('be.visible')
-       
-        cy.get('[style="--section-size:386px; --grid-gap:10px; --grid-template-columns:2.03fr 1fr 1fr;"] > .subsections > :nth-child(3) > :nth-child(1) > .BiModulesModule > .BiModulesTypesBase').should('be.visible')
-       
-        cy.get('.dataTables_scrollBody').should('be.visible')
+
+        Listing.locationandGrouplevelElements()
 
     })
+
+    it('Listing on Groups  - C3000', ()=>{
+
+        cy.visit('/admin/account/3854/group/36523/listings_overview/3182')
+        cy.wait(6000)
+
+        Listing.locationandGrouplevelElements()
+
+    })
+
+    it('Account level Required Fields - C3001', ()=>{
+
+        
+        Listing.settingsGear()
+        Listing.enableAllListings()
+        Listing.gotoListingAccount()
+
+        cy.get('[class="SociTooltip no-style fa fa-info-circle"]').first().click()
+
+        cy.get('#soci_tooltip').then( Tooltip =>{
+
+            cy.wrap( Tooltip ).contains('Required Fields Include:')
+            cy.wrap( Tooltip ).contains('Listings Name')
+            cy.wrap( Tooltip ).contains('Phone')
+            cy.wrap( Tooltip ).contains('Address')
+            cy.wrap( Tooltip ).contains('Primary Category')
+
+        } ).should('be.visible')
+
+
+    } )
+
+    it.only('Required fields update - C3002', ()=>{
+
+        Listing.gotoListingAccount()
+
+        // cy.contains('.frac-numerator', '12')
+
+        cy.get('.frac-numerator').first().should( ($numerator)=>{
+
+            text = $numerator.text()
+
+        })
+
+        
+        cy.get('[style="--section-size:410px; --grid-gap:10px; --grid-template-columns:2.02fr 1fr 1fr;"] > .subsections > :nth-child(3) > :nth-child(1) > .BiModulesModule > .BiModulesTypesBase').click()
+        cy.wait(7000)
+        cy.get('.bbm-modal__section').contains('Smoke-22').click()
+        cy.wait(5000)
+        cy.get('.DlgProject > .bbm-modal > .bbm-modal__section').find('.listings_name').type('L-Name')
+        cy.get('.DlgProject > .bbm-modal > .bbm-modal__bottombar').find('.btn_save').click()
+        cy.wait(4000)
+        cy.reload()
+        cy.wait(7000)
+
+        Listing.gotoListingAccount()
+
+        cy.get('.frac-numerator').first().should( ($numeratornxt)=>{
+
+         textafter = $numeratornxt.text()
+
+         expect(textafter).not.to.eq(text)
+
+      })
+
+
+    })
+
+
+    it.only('Going back a step', ()=>{
+
+         //going back a step
+
+
+         cy.get('[style="--section-size:410px; --grid-gap:10px; --grid-template-columns:2.02fr 1fr 1fr;"] > .subsections > :nth-child(3) > :nth-child(1) > .BiModulesModule > .BiModulesTypesBase').click()
+         cy.wait(7000)
+         cy.get('.bbm-modal__section').contains('Smoke-22').click()
+         cy.wait(7000)
+         cy.get('.DlgProject > .bbm-modal > .bbm-modal__section').find('.listings_name').type('{selectAll}').type('{backspace}')
+         cy.wait(4000)
+         cy.get('.DlgProject > .bbm-modal > .bbm-modal__bottombar').find('.btn_save').click()
+         cy.wait(4000)
+         
+    })
+
+
+    
 
 
 
--- cypress/support/page_objects/Listings.js
+++ cypress/support/page_objects/Listings.js
@@ -2,15 +2,28 @@ export class ListingPage{
 
     settingsGear(){
 
-        cy.reload()
-        cy.wait(5000)
+        cy.visit('/admin/account/3854')
+        cy.wait(6000)
         cy.get('.project_controls > .icon_button > .fa').click()
 
     }
 
+    enableAllListings(){
+
+        cy.wait(5000)
+        cy.get('.btn_enable_all_listings').click()
+        cy.get('.DlgUtilityConfirm > .bbm-modal > .bbm-modal__bottombar > .primary_button').click()
+    }
+
+    disableAllListings(){
+
+        cy.get('.btn_disable_all_autosync').click()
+        cy.get('.DlgUtilityConfirm > .bbm-modal > .bbm-modal__bottombar > .primary_button').click()
+    }
+
     gotoListingAccount(){
 
-        cy.reload()
+        cy.visit('/admin/account/3854')
         cy.wait(6000)
         cy.contains('div[data-href="listings_header"] > .section-heading > .section-label','Listings').click()
         cy.wait(6000)
@@ -25,5 +38,14 @@ export class ListingPage{
         cy.wait(6000)
     }
 
+    locationandGrouplevelElements(){
+
+        cy.get('.highcharts-background').should('be.visible')
+        cy.get('[style="--section-size:auto; height: auto; --grid-gap:10px; --grid-template-columns:1fr 1fr 1fr 1fr;"] > .subsections > :nth-child(4) > .LayoutDropAreaView > .BiModulesModule > .BiModulesTypesBase').should('be.visible')
+        cy.get('[style="--section-size:386px; --grid-gap:10px; --grid-template-columns:2.03fr 1fr 1fr;"] > .subsections > :nth-child(3) > :nth-child(1) > .BiModulesModule > .BiModulesTypesBase').should('be.visible')
+        cy.get('.multi_value_container').should('be.visible')
+        cy.get('.dataTables_scrollBody').should('be.visible')
+
+    }
 
 }
