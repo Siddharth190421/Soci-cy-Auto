63ac05b43dcae98bfc6627a90d79a05f05c29036
Siddharth190421 <siddharth.d@testriq.com>
2022-09-14 17:53:07 +0530

    API waits 14/09

--- cypress/e2e/Admin.cy.js
+++ cypress/e2e/Admin.cy.js
@@ -5,6 +5,25 @@ const loginPage = new LoginPage()
 
 it('login' , ()=>{
 
+
+        //  const logout = (() => {
+
+        cy.request('POST', '/api/logout').then((response) => {
+            if (response.status !== 200) {
+                console.log(response);
+                const bodyStr = JSON.stringify(response.body);
+                throw new Error(
+                    `[cy.login: logout did not have status 200 ${bodyStr}`
+                );
+            }
+            // sometimes logout has status error "You can not do this."
+            if (response.body.status !== 'ok') {
+                console.log('Logout error:', response);
+            }
+        });
+        
+//     });
+
         loginPage.toURL()
         loginPage.userName()
         loginPage.password()
--- cypress/e2e/ads.cy.js
+++ cypress/e2e/ads.cy.js
@@ -43,6 +43,16 @@ describe('Ads', () => {
 
         })
 
+        cy.intercept('GET', '/admin/account/3854/office/0/project/320406/ads_dashboard').as('ads')
+        cy.intercept('GET', '/admin/account/3854/office/0/project/320406/ads_creatives').as('creatives')
+        cy.intercept('GET', '/admin/account/3854/office/0/project/320406/ads_leads').as('leads')
+        cy.intercept('GET', '/admin/account/3854/office/0/project/320406/ads_audiences').as('audience')
+        cy.intercept('GET', '/admin/account/3854/office/0/project/320406/ads_management').as('management')
+
+        cy.intercept('GET', '/api/admin/139546/get_libraries_accessible?*').as('afterCreative')
+        cy.intercept('POST', '/api/admin/139546/get_libraries_accessible?*').as('libdrp')
+        // alternative_ordering=1&type=creative&start=0&limit=20&permission=read&include_total=1&previousStart=0&previousLimit=20&currentView=visual&request_token=wnPmHMIvQ7Vgu52Y8oA8
+        // request_token=wnPmHMIvQ7Vgu52Y8oA8
 
 
     })
@@ -53,7 +63,7 @@ describe('Ads', () => {
 
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_dashboard')
-            cy.wait(6000)
+            cy.wait('@ads')
             cy.get('.date-range-container').click()
             cy.get('[data-range-key="Last Year"]').click()
             cy.get('.dashboard_controls').should('be.visible')
@@ -76,29 +86,30 @@ describe('Ads', () => {
 
         it('Creatve Lib - C2793', () => {
 
-        
+
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_creatives')
-            cy.wait(5000)
+            cy.wait('@creatives')
             cy.get('.btn_add_library').click()
             cy.get('#name').type(LibName)
             cy.get('.primary_button').click()
-            cy.wait(5000)
+            cy.wait('@afterCreative')
 
         })
 
         it('Add creative - C2794', () => {
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_creatives')
+            cy.wait('@creatives')
             cy.contains('.select2-chosen', 'All libraries').click()
-            cy.wait(6000)
+            cy.wait('@libdrp')
             cy.get('#s2id_autogen2_search').type(LibName)
-            cy.wait(7000)
+            cy.wait(4000)
             cy.get('[class="select2-result-label"][role="option"]').first().click()
             cy.get('.btn_add_creative').click()
             cy.get('.AdCreativeImageFormatView > .btn_container > .left-col > .label_check').click()
             cy.get('.primary_button').click({ force: true })
-            cy.wait(5000)
+            cy.wait(2000)
 
         })
 
@@ -108,13 +119,12 @@ describe('Ads', () => {
             cy.get('.AdCreativeImageFormatView > .text_input_container > :nth-child(1) > textarea').first().type('arsenal')
             cy.get('.primary_button').click({ force: true })
 
-
         })
 
         it('Transfer creative - C2796', () => {
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_creatives')
-            cy.wait(5000)
+            cy.wait('@creatives')
             // cy.get(':nth-child(3) > .wrapper > .section_title > .title_box > .truncate')
             cy.get(':nth-child(3) > .wrapper').click()
             cy.wait(4000)
@@ -122,7 +132,7 @@ describe('Ads', () => {
             cy.wait(4000)
             cy.get('.label_checkbox').click()
             cy.contains('.select2-chosen', 'All libraries').click().type('...Lib17thfeb')
-            cy.wait(5000)
+            cy.wait(2000)
             cy.contains('[class="name"]', '...Lib17thfeb').click()
             cy.get('.primary_button').click()
 
@@ -138,6 +148,7 @@ describe('Ads', () => {
 
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_leads')
+            cy.wait('@leads')
             cy.get('[data-href="ads_leads"]').click()
             cy.get('.control_btn_group > .AdLeadsControlBarView > .icon_button').click()
             cy.get(':nth-child(1) > .row > input').type('Leadz' + Hdate + xMen[Math.floor(Math.random() * 6)])
@@ -157,12 +168,12 @@ describe('Ads', () => {
             Cypress.on('fail', (error, runnable) => {
 
                 return false
-                
-              })
-    
+
+            })
+
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_leads')
-            cy.wait(5000)
+            cy.wait('@leads')
             cy.get('[class="icon_button export fa fa-download"][title="Export Leads"]').first().click()
             cy.wait(5000)
             cy.contains('Download All Leads').click()
@@ -174,7 +185,7 @@ describe('Ads', () => {
         it('Leads Form-Copy - C2802', () => {
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_leads')
-            cy.wait(5000)
+            cy.wait('@leads')
             cy.get('[class="icon_button copy fa fa-copy"][title="Copy Form"]').first().click()
             cy.wait(5000)
             cy.get('.primary_button').click()
@@ -185,7 +196,7 @@ describe('Ads', () => {
         it('Leads Form-Delete', () => {
 
             cy.visit('/admin/account/3854/office/0/project/320406/ads_leads')
-            cy.wait(5000)
+            cy.wait('@leads')
             cy.get('[class="icon_button delete fa fa-trash"][title="Delete Form"]').first().click()
             cy.wait(5000)
             cy.get('.primary_button').click()
@@ -199,6 +210,7 @@ describe('Ads', () => {
             it('Audience - C2792', () => {
 
                 cy.get('[data-href="ads_audiences"]').click({ force: true })
+                cy.wait('@audience')
                 cy.get('.btn_create_audience').click()
                 cy.get('.audience_name').type('Audience-' + Hdate)
                 cy.wait(4000)
@@ -213,7 +225,7 @@ describe('Ads', () => {
 
 
                 cy.visit('/admin/account/3854/office/0/project/320406/ads_management')
-                cy.wait(5000)
+                cy.wait('@management')
                 cy.get('.control_btn_group > .active').click()
                 cy.get('.campaign > .field_input > .promote_input').type('Oranges')
                 cy.contains('Custom Objective Per Ad').click()
@@ -236,7 +248,7 @@ describe('Ads', () => {
             it('Pause Ads - C2798', () => {
 
                 cy.visit('/admin/account/3854/office/0/project/320406/ads_management')
-                cy.wait(6000)
+                cy.wait('@management')
                 cy.get('.dropdown').click()
                 cy.get('.overflow_menu_item').click()
 
