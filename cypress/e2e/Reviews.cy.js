964ccdf22e243828a1165a82bf2116bb732a6507
unknown <siddharth.d@testriq.com>
2024-01-03 18:10:02 +0530

    Reviews 03/01

--- cypress/e2e/Reviews.cy.js
+++ cypress/e2e/Reviews.cy.js
@@ -15,12 +15,35 @@ describe('Reviews', ()=>{
         cy.get('.report_view_container').should('be.visible')
         cy.wait(4000)
         cy.get('.highcharts-series.highcharts-series-0 > [x="132.5"]').should('be.visible')
-        cy.get('#highcharts-1w0sa0o-92 > .highcharts-root > .highcharts-background').should('be.visible')
-
-        
-        
+        cy.get('.report_view_container').should('be.visible')
 
 
     })
+
+    it("Inbox",()=>{
+
+        cy.get('[data-href="reputation_reviews"]').click()
+        cy.get('[data-name="reviews"] > :nth-child(1) > .component_tab_text').click()
+        cy.wait(7000)
+        cy.get('[ref="reviews"] > :nth-child(1)').should('be.visible')
+
+    })
+
+    it('More', ()=>{
+
+        cy.get('.label').click()
+        cy.wait(5000)
+        cy.get('[data-name="rules"] > :nth-child(1) > .component_tab_text').click()
+        cy.get('[data-name="my_tasks"]').should('be.visible')
+
+    })
+
+    it('Locations',()=>{
+
+        cy.get('[data-href="reputation_locations"]').click()
+        
+    })
+
+
 
 })
